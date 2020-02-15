126254d775eeff75aa777a42533275d08d696662
Facova <fsodilao@gmail.com>
2020-02-15 18:16:40 -0300

    Pieces King and Rook

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import javafx.geometry.Pos;
+
 public class Board {
 
     private int rows;
@@ -35,4 +37,10 @@ public class Board {
     public Piece piece(Position position) {
         return pieces[position.getRow()][position.getColumn()];
     }
+
+    public void placePiece(Piece piece, Position position) {
+            pieces[position.getRow()][position.getColumn()] = piece;
+            piece.position = position;
+    }
+
 }
